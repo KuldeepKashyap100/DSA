5c075ed4cc1f967554613609b6b7a6b63c82492f
Kumar <kumark@pun-kumark-m.ds.ad.adp.com>
2020-09-30 09:07:34 +0530

    link list problems completed

--- linked_list_problems/sum_of_elements_of_two_link_list.js
+++ linked_list_problems/sum_of_elements_of_two_link_list.js
@@ -1,26 +1,59 @@
 const LinkedList = require("../linked_list/LinkedList");
 
-const addListNumbersWrapper = (firstList, secondList) => {
-    let result = new LinkedList();
-    const firstListLength = firstList.totalElements;
-    const secondListLength = secondList.totalElements;
+const addListNumbersWrapper = (first, second) => {
+    let resultList = new LinkedList();
+    let firstList = first.head;
+    let secondList = second.head;
+
+    const firstListLength = first.totalElements;
+    const secondListLength = second.totalElements;
 
     if(secondListLength>firstListLength)
         [firstList, secondList] = [secondList, firstList];
 
+    let longerList = firstList;
     let diff = Math.abs(firstListLength-secondListLength);
     while(diff--) firstList = firstList.next;
 
-    addListNumbers(firstList, secondList)
+    let carry = {carryNum: 0}
+    addListNumbers(firstList, secondList, resultList, carry);
+    // resultList.reverse();
 
+    diff = Math.abs(firstListLength-secondListLength);
+    addRemainingNumbers(longerList, carry, resultList, diff);
+
+    if(carry.carryNum) {
+        resultList.insert(carry.carryNum);
+    }
+
+    resultList.reverse();
+    return resultList;
 }
 
-const addListNumbers = (firstList, secondList) => {
-    
+const addListNumbers = (firstList, secondList, resultList, carry) => {
+    let sum;
+    if(!firstList)
+        return;
+    addListNumbers(firstList.next, secondList.next, resultList,carry);
+
+    //end of both lists
+    sum = firstList.data + secondList.data + carry.carryNum;
+    carry.carryNum = Math.floor(sum/10);
+    sum = sum % 10;
+    resultList.insert(sum);
+    return;
 }
 
-const addRemainingNumbers = (head, carry) => {
-
+const addRemainingNumbers = (firstList, carry, resultList, diff) => {
+    let sum;
+    if(!firstList || !diff)
+        return;
+    addRemainingNumbers(firstList.next, carry, resultList, diff-1);
+    sum = firstList.data + carry.carryNum;
+    carry.carryNum =  Math.floor(sum/10);
+    sum = sum % 10;
+    resultList.insert(sum);
+    return;
 }
 
 let firstList = new LinkedList();
@@ -31,7 +64,7 @@ firstList.insert(4);
 firstList.insert(5);
 
 let secondList = new LinkedList();
-secondList.insert(6);
+// secondList.insert(6);
 secondList.insert(7);
 secondList.insert(8);
 secondList.insert(9);
@@ -44,5 +77,5 @@ secondList.insert(10);
 // firstList.display();
 // secondList.display();
 
-let result = addListNumbersWrapper(firstList.head, secondList.head);
-result.display();
+let resultList = addListNumbersWrapper(firstList, secondList);
+resultList.display();
