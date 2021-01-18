838bca9b15dc282e137b8483741c88db32f52e2c
Kumar <kumark@pun-kumark-m.ds.ad.adp.com>
2021-01-18 15:01:06 +0530

    selection algo completed

--- string/robinKarpAlgo.js
+++ string/robinKarpAlgo.js
@@ -1,3 +1,5 @@
+// time -> O(nm) but if hashing function takes constant time then -> o(n) [complexity totally depends upon hashing function]
+// space -> O(1)
 const robinKarp = (text, pattern) => {
     const textHash = calculateHash(text, 0, pattern.length - 1);
     const patternHash = calculateHash(pattern, 0, pattern.length - 1);
@@ -8,8 +10,9 @@ const robinKarp = (text, pattern) => {
 
     let newHash = textHash;
     for(let i = 1; i <= text.length - pattern.length; i++) {
+        newHash = reCalculateHash(text, i - 1, i + pattern.length - 1, newHash,pattern.length);
         if(patternHash === newHash) {
-            let j = i - (pattern.length - 1);
+            let j = 0;
             while(j < pattern.length && text[i + j] === pattern[j])
             j++;
             if(pattern.length === j) {
@@ -17,7 +20,6 @@ const robinKarp = (text, pattern) => {
                 return;
             }
         }
-        newHash = reCalculateHash(text, i - 1, i + pattern.length - 1, newHash,pattern.length);
     }
     console.log("not found");
 }
@@ -26,25 +28,25 @@ const calculateHash = (input, start, end) => {
     let hash = 0;
     for(let i = start; i <= end; i++) {
         hash *= 10;
-        // hash += input.charCodeAt(i);
-        hash += map[input[i]];
+        hash += input.charCodeAt(i);
+        // hash += map[input[i]];
     }
     return hash;
 }
 
-const reCalculateHash = (input, start, newEnd, currentHash, patternLength) => {
-    // const newHash = (currentHash - (Math.pow(10, patternLength - 1) * input.charCodeAt(start))) * 10 + input.charCodeAt(newEnd);
-    const newHash = (currentHash - (Math.pow(10, patternLength - 1) * map[input[start]])) * 10 + map[input[newEnd]];
+const reCalculateHash = (input, oldStart, newEnd, currentHash, patternLength) => {
+    const newHash = (currentHash - (Math.pow(10, patternLength - 1) * input.charCodeAt(oldStart))) * 10 + input.charCodeAt(newEnd);
+    // const newHash = (currentHash - (Math.pow(10, patternLength - 1) * map[input[oldStart]])) * 10 + map[input[newEnd]];
     return newHash;
 }
 
-const map = {
-    a: 1,
-    b: 2,
-    c: 3
-}
+// const map = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
 
-const text = "abcaabbab";
-const pattern = "bb";
+const text = "acabdc";
+const pattern = "aca";
 
 robinKarp(text, pattern);
