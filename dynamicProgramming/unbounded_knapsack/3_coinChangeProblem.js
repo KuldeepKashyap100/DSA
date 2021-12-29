6bd7038f6e7c78160694b69c0e2f2da9db2f4f73
Kuldeep <kuldeep.kumar@cdk.com>
2021-12-29 14:34:55 +0530

    coin problem alter sol

--- dynamicProgramming/unbounded_knapsack/3_coinChangeProblem.js
+++ dynamicProgramming/unbounded_knapsack/3_coinChangeProblem.js
@@ -15,6 +15,38 @@
  * It is similar to countSubsetSum but for unbounded knapsack.
  */
 
+ function numberOfWaysToMakeChange(coins, targetSum, idx) {
+	if(targetSum < 0) return 0;
+	if(targetSum === 0) return 1;
+	
+	let totalNumOfWays = 0;
+	for(let coinIdx = idx; coinIdx < coins.length; coinIdx++) {
+		const denom = coins[coinIdx];
+		totalNumOfWays += numberOfWaysToMakeChange(coins, targetSum - denom, coinIdx);
+	}
+	
+	return totalNumOfWays;
+}
+
+// time -> O(nd) | space -> O(n)
+function numberOfWaysToMakeChange(n, denoms) {
+    const ways = new Array(n + 1).fill(0);
+      ways[0] = 1;
+      
+      for(const denom of denoms) {
+          for(let amount = 1; amount <= n; amount++) {
+              if(denom <= amount) {
+                  ways[amount] += ways[amount - denom];
+              }
+          }
+      }
+      return ways[n];
+  }
+  
+  // Do not edit the line below.
+  exports.numberOfWaysToMakeChange = numberOfWaysToMakeChange;
+  
+
 const coinChangeProblem = (coins, sum) => {
     const table = new Array(coins.length + 1).fill().map(_ => new Array(sum + 1).fill(0));
     for(let i = 0; i <= coins.length; i++) table[i][0] = 1;
